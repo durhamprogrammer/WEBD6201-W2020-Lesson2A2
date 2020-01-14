ce3967fff9f06500226c8bd3af16b7ddbb3c9047
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-01-14 16:06:57 -0500

    created named Event Handlers

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,35 +1,46 @@
+"use strict";
+
 // IIFE - Immediately Invoked Function Expression
 // means -> anonymous self-executing function
 (function(){
+    // IIFE Scope
+    let h2;
+
+      
+
+    // Event Handlers
+    let MouseOver = function()
+    {
+        // MouseOver Scope
+        h2.style.color = "blue";
+        h2.style.fontWeight = "bold";
+        h2.textContent = "Mouse Over!!!";
+    }
+
+    function MouseOut()
+    {
+        h2.style.color = "black";
+        h2.style.fontWeight = "normal";
+        h2.textContent = "My Text Content";
+    }
 
     function Start()
     {
+        // Start Scope
+
         console.log("%c App Started", "color: blue; font-size: 20px;");
         let mainContent = document.getElementById("mainContent");
 
         // create a new h2 tag with a class of Display-2 and "My Text Content" as the text node
-        let h2 = document.createElement("h2");
+        h2 = document.createElement("h2");
         h2.setAttribute("class","Display-2");
         h2.textContent = "My Text Content";
         mainContent.appendChild(h2);
 
-        h2.addEventListener("mouseover", function(){
-            h2.style.color = "blue";
-            h2.style.fontWeight = "bold";
-        });
+        // event Listeners
+        h2.addEventListener("mouseover", MouseOver);
 
-        h2.addEventListener("mouseover", function(){
-            h2.textContent = "Mouse Over!!!";
-        });
-
-        h2.addEventListener("mouseout", function(){
-            h2.style.color = "black";
-            h2.style.fontWeight = "normal";
-        });
-
-        h2.addEventListener("mouseout", function(){
-            h2.textContent = "My Text Content";
-        });
+        h2.addEventListener("mouseout", MouseOut);
 
         let brand = document.getElementsByClassName("navbar-brand")[0];
         brand.textContent = "Client-Side Scripting";
